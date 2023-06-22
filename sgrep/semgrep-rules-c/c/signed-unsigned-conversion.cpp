78f579f07dc0260576e0cd681c4692b3f6f300bb
jetp1ane <jamesbarnett1128@gmail.com>
2023-06-22 00:29:08 -0600

    upd01

---
+++ sgrep/semgrep-rules-c/c/signed-unsigned-conversion.cpp
@@ -0,0 +1,16 @@
+// Marco Ivaldi <raptor@0xdeadbeef.info>
+
+#include <sys/types.h>
+
+SharedMemory* SharedMemory::New(Plugin* plugin, off_t length)
+{
+	void* map_addr = NULL;
+	// ruleid: raptor-signed-unsigned-conversion
+	size_t size = static_cast<size_t>(length);
+	NaClHandle handle = nacl::CreateMemoryObject(size);
+	struct NaClDescImcShm *imc_desc = reinterpret_cast <struct NaClDescImcShm*>(malloc( sizeof (*imc_desc)));
+	struct NaClDesc* desc = reinterpret_cast<struct NaClDesc*>(imc_desc);
+	dprintf(("SharedMemory::New(%p, 0x%08x)\n ", plugin, (unsigned) length));
+	NaClDescImcShmCtor(imc_desc, handle, length);
+	return New(plugin, desc);
+}
